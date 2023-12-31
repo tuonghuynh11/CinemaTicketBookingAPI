0f0e118769873002c4d0cae3fde98d114b663fb6
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-12-31 21:11:57 +0700

    *

--- CinemaTicketBooking.Server/Scaffolds/Models/EntityLayer/Movies.cs
+++ CinemaTicketBooking.Server/Scaffolds/Models/EntityLayer/Movies.cs
@@ -3,96 +3,96 @@ using System.Text.Json.Serialization;
 
 namespace CinemaTicketBooking.Server.Scaffolds.Models.EntityLayer
 {
-	public class Movies : BaseEntity
-	{
-		public Movies() : base()
-		{
-		}
+    public class Movies : BaseEntity
+    {
+        public Movies() : base()
+        {
+        }
 
-		public Movies(long id) : base()
-		{
-			Id = id;
-		}
+        public Movies(long id) : base()
+        {
+            Id = id;
+        }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public bool? Adult { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public bool? Adult { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? BackdropPath { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? BackdropPath { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? BelongsToCollection { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? BelongsToCollection { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public long? Budget { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public long? Budget { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? Genres { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Genres { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? Homepage { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Homepage { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public long? Id { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public long? Id { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? ImdbId { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? ImdbId { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? OriginalLanguage { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? OriginalLanguage { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? OriginalTitle { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? OriginalTitle { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? Overview { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Overview { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? Popularity { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public decimal? Popularity { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? PosterPath { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? PosterPath { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? ProductionCompanies { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? ProductionCompanies { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? ProductionCountries { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? ProductionCountries { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? ReleaseDate { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? ReleaseDate { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public long? Revenue { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public long? Revenue { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public long? Runtime { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public long? Runtime { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? SpokenLanguages { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? SpokenLanguages { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? Status { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Status { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? Tagline { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Tagline { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public string? Title { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Title { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public bool? Video { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public bool? Video { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? VoteAverage { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public decimal? VoteAverage { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public long? VoteCount { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public long? VoteCount { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? Casting { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Casting { get; set; }
 
-		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-		public object? Directors { get; set; }
-	}
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string? Directors { get; set; }
+    }
 }
